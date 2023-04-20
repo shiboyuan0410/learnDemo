da31f20adf0ccc8b25ce20552f0d55f97bcc6b9f
s3986483 <shiboyuan0410@163.com>
2023-04-20 11:43:52 +0800

    [feat]: first commit

--- src/main/java/com/example/learndemo/mq/config/RabbitMqConfig.java
+++ src/main/java/com/example/learndemo/mq/config/RabbitMqConfig.java
@@ -3,7 +3,6 @@ package com.example.learndemo.mq.config;
 import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * @Author: shiboyuan
